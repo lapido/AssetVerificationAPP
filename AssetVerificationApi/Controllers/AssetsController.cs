ee3748f9548d8d0c76556abdbfb0658cf3300d33
Victor Abidoye <victortemitope95@gmail.com>
2018-07-13 07:54:08 +0100

    Update AssetsController.cs

--- AssetVerificationApi/Controllers/AssetsController.cs
+++ AssetVerificationApi/Controllers/AssetsController.cs
@@ -1,4 +1,4 @@
-﻿using System.Linq;
+using System.Linq;
 using System.Web.Http;
 using AssetVerificationApi.Context_;
 using AssetVerificationApi.Models;
@@ -29,6 +29,54 @@ namespace AssetVerificationApi.Controllers
             return Ok();
         }
 
+        
+        [HttpPost]
+        [Route("api/forgotPassword")]
+        public IHttpActionResult ForgotPassword(JObject AssetObj)
+        {
+            var username = AssetObj["userName"].ToString();
+            var newPassword = AssetObj["newPassword"].ToString();
+            var secretAnswer = AssetObj["secretAnswer"].ToString();
+            var user = context.UserModel.Where(x => x.Username == username).FirstOrDefault<UserModel>();
+            if (user != null)
+            {
+                if (user.HasChangedPassword == 0)
+                {
+                    ////Todo: check if secret answer exist
+
+
+                    //user.HasChangedPassword = 1;
+                    //user.Password = newPassword;
+                    //user.SecretAnswer = secretAnswer;
+
+                    return Unauthorized();
+                }
+                else if(user.HasChangedPassword == 1)
+                {
+                    //check if secret answer is the same with the one stored in the DB
+                    if (user.SecretAnswer.Equals(secretAnswer))
+                    {
+                        user.Password = newPassword;
+
+                        //return Ok(1);
+                    }
+                    else
+                    {
+                        return Ok(2);
+                    }
+                }
+            }
+            else
+            {
+                return NotFound();
+            }
+
+            context.Entry(user).State = EntityState.Modified;
+            context.SaveChanges();
+
+            return Ok(1);
+        }
+
         [Authorize]
         [HttpPost]
         [Route("api/changePassword")]
@@ -56,25 +104,25 @@ namespace AssetVerificationApi.Controllers
                     //return Ok(1);
                 }
 
-                //changing password after the initial required change
-                else if(user.HasChangedPassword == 1)
-                {
-                    //check if secret answer is the same with the one stored in the DB
-                    if (user.SecretAnswer.Equals(secretAnswer))
-                    {
-                        user.Password = newPassword;
+                ////changing password after the initial required change
+                //else if(user.HasChangedPassword == 1)
+                //{
+                //    //check if secret answer is the same with the one stored in the DB
+                //    if (user.SecretAnswer.Equals(secretAnswer))
+                //    {
+                //        user.Password = newPassword;
 
-                        //return Ok(1);
-                    }
-                    else
-                    {
-                        return Ok(2);
-                    }
-                }
+                //        //return Ok(1);
+                //    }
+                //    else
+                //    {
+                //        return Ok(2);
+                //    }
+                //}
             }
             else
             {
-                return Ok(0);
+                return NotFound();
             }
 
             context.Entry(user).State = EntityState.Modified;
@@ -104,16 +152,18 @@ namespace AssetVerificationApi.Controllers
 
             var ParentAssetID = Int32.Parse(AssetObj["ParentAssetID"].ToString());
             var ChildID = Int32.Parse(AssetObj["ChildID"].ToString());
-            var SiteID = Int32.Parse(AssetObj["SiteID"].ToString());
+            var lat = Decimal.Parse(AssetObj["latitude"].ToString());
+            var log = Decimal.Parse(AssetObj["longitude"].ToString());
+            //var SiteID = Int32.Parse(AssetObj["SiteID"].ToString());
             var username = AssetObj["username"].ToString();
-            var uniqueIdentifier = AssetObj["UniqueIdentifier"].ToString();
+            //var uniqueIdentifier = AssetObj["UniqueIdentifier"].ToString();
             var UserID = context.UserModel.Where(x => x.Username == username).FirstOrDefault<UserModel>().UserId;
 
             //store in the asset model
             AssetModel assetModel = new AssetModel()
             {
-                SiteID = SiteID,
-                UniqueIdentifier = uniqueIdentifier,
+                //SiteID = SiteID,
+                //UniqueIdentifier = uniqueIdentifier,
                 ParentAssetID = ParentAssetID,
                 ChildID = ChildID
             };
@@ -138,7 +188,8 @@ namespace AssetVerificationApi.Controllers
             FieldDataModel fieldData = new FieldDataModel()
             {
                 AssetID = assetModel.AssetID,
-                SiteID = SiteID,
+                Latitude = lat,
+                Longitude = log,
                 ParentAssetID = ParentAssetID,
                 ChildID = ChildID,
                 UserID = UserID
