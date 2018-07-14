1d002802d86cf95fd93e1f029f8d4d28f57904bf
Victor Abidoye <swissvic95@gmail.com>
2018-07-14 01:03:48 +0100

    Update AssetsController.cs

--- AssetVerificationApi/Controllers/AssetsController.cs
+++ AssetVerificationApi/Controllers/AssetsController.cs
@@ -42,6 +42,13 @@ namespace AssetVerificationApi.Controllers
             {
                 if (user.HasChangedPassword == 0)
                 {
+                    ////Todo: check if secret answer exist
+
+
+                    //user.HasChangedPassword = 1;
+                    //user.Password = newPassword;
+                    //user.SecretAnswer = secretAnswer;
+
                     return Unauthorized();
                 }
                 else if(user.HasChangedPassword == 1)
@@ -87,11 +94,31 @@ namespace AssetVerificationApi.Controllers
                 //this is the first time of changing password has required by the system
                 if(user.HasChangedPassword == 0)
                 {
+                    //Todo: check if secret answer exist
+
+
                     user.HasChangedPassword = 1;
                     user.Password = newPassword;
                     user.SecretAnswer = secretAnswer;
 
+                    //return Ok(1);
                 }
+
+                ////changing password after the initial required change
+                //else if(user.HasChangedPassword == 1)
+                //{
+                //    //check if secret answer is the same with the one stored in the DB
+                //    if (user.SecretAnswer.Equals(secretAnswer))
+                //    {
+                //        user.Password = newPassword;
+
+                //        //return Ok(1);
+                //    }
+                //    else
+                //    {
+                //        return Ok(2);
+                //    }
+                //}
             }
             else
             {
@@ -120,61 +147,66 @@ namespace AssetVerificationApi.Controllers
         [Authorize]
         [HttpPost]
         [Route("api/postAsset")]
-        public IHttpActionResult PostVerify(JObject AssetObj)
+        public IHttpActionResult PostVerify(IEnumerable<JObject> AssetObjs)
         {
-
-            var ParentAssetID = Int32.Parse(AssetObj["ParentAssetID"].ToString());
-            var ChildID = Int32.Parse(AssetObj["ChildID"].ToString());
-            var lat = Decimal.Parse(AssetObj["latitude"].ToString());
-            var log = Decimal.Parse(AssetObj["longitude"].ToString());
-            //var SiteID = Int32.Parse(AssetObj["SiteID"].ToString());
-            var username = AssetObj["username"].ToString();
-            //var uniqueIdentifier = AssetObj["UniqueIdentifier"].ToString();
-            var UserID = context.UserModel.Where(x => x.Username == username).FirstOrDefault<UserModel>().UserId;
-
-            //store in the asset model
-            AssetModel assetModel = new AssetModel()
+            foreach(var AssetObj in AssetObjs)
             {
-                //SiteID = SiteID,
-                //UniqueIdentifier = uniqueIdentifier,
-                ParentAssetID = ParentAssetID,
-                ChildID = ChildID
-            };
+                var ParentAssetID = Int32.Parse(AssetObj["ParentAssetID"].ToString());
+                var ChildID = Int32.Parse(AssetObj["ChildID"].ToString());
+                var lat = Decimal.Parse(AssetObj["latitude"].ToString());
+                var log = Decimal.Parse(AssetObj["longitude"].ToString());
+                //var SiteID = Int32.Parse(AssetObj["SiteID"].ToString());
+                var username = AssetObj["username"].ToString();
+                //var uniqueIdentifier = AssetObj["UniqueIdentifier"].ToString();
+                var UserID = context.UserModel.Where(x => x.Username == username).FirstOrDefault<UserModel>().UserId;
 
-            //storing the properties value into the dB
-            var Properties = AssetObj["Properties"];
-            foreach(var property in Properties)
-            {
-                PropertyValue prop = new PropertyValue()
+                //store in the asset model
+                AssetModel assetModel = new AssetModel()
+                {
+                    //SiteID = SiteID,
+                    //UniqueIdentifier = uniqueIdentifier,
+                    ParentAssetID = ParentAssetID,
+                    ChildID = ChildID
+                };
+
+                //storing the properties value into the dB
+                var Properties = AssetObj["Properties"];
+                foreach (var property in Properties)
+                {
+                    PropertyValue prop = new PropertyValue()
+                    {
+                        AssetID = assetModel.AssetID,
+                        ParentAssetID = ParentAssetID,
+                        ChildID = ChildID,
+                        PropertyID = Int32.Parse(property["PropertyID"].ToString()),
+                        Value = property["Value"]["Name"].ToString(),
+                        //OptionID = Int32.Parse(property["Value"]["OptionID"].ToString()),
+                    };
+                    context.PropertyValue.Add(prop);
+                }
+
+                //Field Data
+                FieldDataModel fieldData = new FieldDataModel()
                 {
                     AssetID = assetModel.AssetID,
+                    Latitude = lat,
+                    Longitude = log,
                     ParentAssetID = ParentAssetID,
                     ChildID = ChildID,
-                    PropertyID = Int32.Parse(property["PropertyID"].ToString()),
-                    Value = property["Value"]["Name"].ToString(),
-                    //OptionID = Int32.Parse(property["Value"]["OptionID"].ToString()),
+                    UserID = UserID
                 };
-                context.PropertyValue.Add(prop);
+
+
+                context.AssetModel.Add(assetModel);
+                context.FieldDataModel.Add(fieldData);
+
+
+
+                context.SaveChanges();
             }
-
-            //Field Data
-            FieldDataModel fieldData = new FieldDataModel()
-            {
-                AssetID = assetModel.AssetID,
-                Latitude = lat,
-                Longitude = log,
-                ParentAssetID = ParentAssetID,
-                ChildID = ChildID,
-                UserID = UserID
-            };
-
-
-            context.AssetModel.Add(assetModel);
-            context.FieldDataModel.Add(fieldData);
-
-            context.SaveChanges();
-
+            
             return Ok();
+
         }
 
 
