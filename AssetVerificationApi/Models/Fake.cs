d0b3cee5f07208d785d0e64570a796e5b433ac3e
Victor Abidoye <swissvic95@gmail.com>
2018-07-15 23:14:38 +0100

    Update Fake.cs

--- AssetVerificationApi/Models/Fake.cs
+++ AssetVerificationApi/Models/Fake.cs
@@ -11,6 +11,6 @@ namespace AssetVerificationApi.Models
         [Key]
         public int ID { get; set; }
         public string Name { get; set; }
-        public string City {get; set;}
+        
     }
 }
