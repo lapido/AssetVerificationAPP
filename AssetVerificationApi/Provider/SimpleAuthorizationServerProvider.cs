d8a8becd1955388d76ba73099e2e1f1491b065ed
Victor Abidoye <swissvic95@gmail.com>
2018-07-16 09:44:10 +0100

    Update SimpleAuthorizationServerProvider.cs

--- AssetVerificationApi/Provider/SimpleAuthorizationServerProvider.cs
+++ AssetVerificationApi/Provider/SimpleAuthorizationServerProvider.cs
@@ -1,13 +1,17 @@
-﻿using Microsoft.Owin.Security;
+using Microsoft.Owin.Security;
 using Microsoft.Owin.Security.OAuth;
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
 
+using Microsoft.Owin.Security.Infrastructure;
+using System.Collections.Concurrent;
+
 using AssetVerificationApi.Context_;
 using System.Web.Http.Cors;
 using AssetVerificationApi.Models;
+using System;
 
 namespace AssetVerificationApi.Provider
 {
@@ -71,5 +75,53 @@ namespace AssetVerificationApi.Provider
             return;
         }
         
+    }
+
+    public class RefreshTokenProvider : IAuthenticationTokenProvider
+    {
+        private static ConcurrentDictionary<string, AuthenticationTicket> _refreshTokens = new ConcurrentDictionary<string, AuthenticationTicket>();
+
+        public async Task CreateAsync(AuthenticationTokenCreateContext context)
+        {
+
+            var guid = Guid.NewGuid().ToString();
+
+            // copy all properties and set the desired lifetime of refresh token  
+            var refreshTokenProperties = new AuthenticationProperties(context.Ticket.Properties.Dictionary)
+            {
+                IssuedUtc = context.Ticket.Properties.IssuedUtc,
+                ExpiresUtc = DateTime.UtcNow.AddMinutes(60)
+            };
+
+            var refreshTokenTicket = new AuthenticationTicket(context.Ticket.Identity, refreshTokenProperties);
+
+            _refreshTokens.TryAdd(guid, refreshTokenTicket);
+
+            // consider storing only the hash of the handle  
+            context.SetToken(guid);
+        }
+
+
+        public void Create(AuthenticationTokenCreateContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Receive(AuthenticationTokenReceiveContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task ReceiveAsync(AuthenticationTokenReceiveContext context)
+        {
+            // context.DeserializeTicket(context.Token);
+            AuthenticationTicket ticket;
+            string header = context.OwinContext.Request.Headers["Authorization"];
+
+            if (_refreshTokens.TryRemove(context.Token, out ticket))
+            {
+                context.SetTicket(ticket);
+            }
+        }
     }
 }
