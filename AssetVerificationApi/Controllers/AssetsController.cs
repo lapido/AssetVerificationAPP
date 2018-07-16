ce668b3a8fc70b7144b8f4ff9fc9b3683a38f346
Victor Abidoye <swissvic95@gmail.com>
2018-07-16 11:18:17 +0100

    Update AssetsController.cs

--- AssetVerificationApi/Controllers/AssetsController.cs
+++ AssetVerificationApi/Controllers/AssetsController.cs
@@ -6,6 +6,8 @@ using System.Data.Entity;
 using Newtonsoft.Json.Linq;
 using System.Collections.Generic;
 using System;
+using System.Dynamic;
+
 
 namespace AssetVerificationApi.Controllers
 {
@@ -209,7 +211,91 @@ namespace AssetVerificationApi.Controllers
             return Ok();
 
         }
+//AdminController endpoints
+        [HttpGet]
+        [Route("api/admin/getGroups")]
+        public IHttpActionResult GetParentAssets()
+        {
+            var parents = context.ParentAsset.ToList();
+            return Ok(parents);
+        }
 
+
+
+        [HttpGet]
+        [Route("api/admin/allAssets")]
+        public IHttpActionResult GetAllAssetsAdmin()
+        {
+            var assets = (from asset in context.AssetModel
+                          join field in context.FieldDataModel on asset.AssetID equals field.AssetID
+                          join parent in context.ParentAsset on asset.ParentAssetID equals parent.ParentAssetID
+                          join child in context.AssetChild on asset.ChildID equals child.ChildID
+                          join user in context.UserModel on field.UserID equals user.UserId
+                          select new
+                          {
+                              AssetGroup = parent.Name,
+                              GroupID = parent.ParentAssetID,
+                              Child = child.Name,
+                              longitude = field.Longitude,
+                              latitude = field.Latitude,
+                              username = user.Name
+                          });
+
+            return Ok(assets);
+        }
+
+
+        [HttpGet]
+        [Route("api/admin/getAssetsForGroup")]
+        public IHttpActionResult GetAssetsForGroup(int groupID)
+        {
+            var assets_ = (from asset in context.AssetModel
+                           join field in context.FieldDataModel on asset.AssetID equals field.AssetID
+                           join property in context.PropertyValue on asset.ChildID equals property.ChildID
+                           join propertyT in context.Property on property.PropertyID equals propertyT.PropertyID
+                           join parent in context.ParentAsset on asset.ParentAssetID equals parent.ParentAssetID
+                           join child in context.AssetChild on asset.ChildID equals child.ChildID
+                           where asset.ParentAssetID == groupID
+                           select new
+                           {
+                               AssetGroup = parent.Name,
+                               Children = child.Name,
+
+                           });
+            var assets = context.AssetModel.Where(x => x.ParentAssetID == groupID).ToList();
+            var children = context.AssetChild.Where(x => x.ParentAssetID == groupID).ToList();
+
+            List<object> AssetsList = new List<object>();
+            foreach (var asset in assets)
+            {
+
+                var assetObj = new ExpandoObject() as IDictionary<string, Object>;
+                foreach (var child in children)
+                {
+                    var properties = context.Property.Where(x => x.ChildID == child.ChildID);
+                    foreach (var p in properties)
+                    {
+
+                        var c = context.PropertyValue.Where(x => x.AssetID == asset.AssetID & x.PropertyID == p.PropertyID).FirstOrDefault();
+                        if (c != null)
+                        {
+
+                            assetObj.Add(p.Name, c.Value);
+                        }
+                        else
+                        {
+                            assetObj.Add(p.Name, "");
+                        }
+
+                    }
+                }
+                AssetsList.Add(assetObj);
+            }
+
+
+
+            return Ok(AssetsList);
+        }
 
     }
 }
