dcef9842cb157cbcad199c58bbbdaefb066257e8
Victor Abidoye <victortemitope95@gmail.com>
2018-07-25 11:49:20 +0100

    Update Context.cs

--- AssetVerificationApi/Context_/Context.cs
+++ AssetVerificationApi/Context_/Context.cs
@@ -30,6 +30,8 @@ namespace AssetVerificationApi.Context_
         {
             modelBuilder.Entity<SiteModel>().Property(a => a.Latitude).HasPrecision(18, 9);
             modelBuilder.Entity<SiteModel>().Property(a => a.Longitude).HasPrecision(18, 9);
+            modelBuilder.Entity<FieldDataModel>().Property(a => a.Longitude).HasPrecision(18, 9);
+            modelBuilder.Entity<FieldDataModel>().Property(a => a.Longitude).HasPrecision(18, 9);
         }
     }
 }
